cf512d332c68ee400931dd337e8f876f15cbdfa8
Mathieu Pavageau <mthpvg@gmail.com>
2020-03-28 10:38:47 +0100

    chore: check fonctions

--- addition/addition.js
+++ addition/addition.js
@@ -3,25 +3,29 @@ const number2Div = document.getElementById('number2')
 const scoreDiv = document.getElementById('score')
 const resultDiv = document.getElementById('result')
 
-let number1 = newNumber()
-let number2 = newNumber()
-let result = ''
 let score = 0
 
+let number1 = null
+let number2 = null
+let result = ''
+
+reset()
 print()
 
 document.addEventListener('keypress', keypress)
 
 function keypress(event) {
   if (event.key === 'Enter') {
-    score += parseInt(result) === number1 + number2 ? 1 : -1
-    number1 = newNumber()
-    number2 = newNumber()
-    result = ''
+    score += check() ? 1 : -1
+    reset()
   } else {
     result += event.key
   }
   print()
+}
+
+function check() {
+  return parseInt(result) === number1 + number2
 }
 
 function print() {
@@ -29,6 +33,12 @@ function print() {
   number2Div.innerHTML = number2
   scoreDiv.innerHTML = score
   resultDiv.innerHTML = result
+}
+
+function reset() {
+  number1 = newNumber()
+  number2 = newNumber()
+  result = ''
 }
 
 function newNumber() {
--- alphabet/alphabet.js
+++ alphabet/alphabet.js
@@ -1,22 +1,40 @@
 const letterDiv = document.getElementById('letter')
+const resultDiv = document.getElementById('result')
 const scoreDiv = document.getElementById('score')
 
-let letter = newLetter()
 let score = 0
 
+let letter = null
+let result = null
+
+reset()
 print()
 
 document.addEventListener('keypress', keypress)
 
 function keypress(event) {
-  score += event.keyCode === letter.charCodeAt() ? 1 : -1
-  letter = newLetter()
+  if (event.key === 'Enter') {
+    score += check() ? 1 : -1
+    reset()
+  } else {
+    result = event.key
+  }
   print()
+}
+
+function check() {
+  return result === letter
 }
 
 function print() {
   scoreDiv.innerHTML = score
   letterDiv.innerHTML = letter.toUpperCase()
+  resultDiv.innerHTML = result.toUpperCase()
+}
+
+function reset() {
+  letter = newLetter()
+  result = ''
 }
 
 function newLetter() {
