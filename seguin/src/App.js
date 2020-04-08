eff9cf01b23c9583db11dedca4a93fb8d262cc86
Pauline <pauline@Mathieus-MacBook-Pro.local>
2020-04-08 15:47:20 +0200

    navbars

--- seguin/src/App.js
+++ seguin/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Redirect, Route, Router } from 'react-router-dom';
+import Navbar from './Navbar';
 import Seguin1Niveau2 from './Seguin1Niveau2';
 import './style.scss'
 const cloudFolder = "https://res.cloudinary.com/eclimontessori/video/upload/v1586180865/audio-application-seguin/"
-const logo = "https://res.cloudinary.com/eclimontessori/image/upload/v1586343660/logo-small_rfd5z6.png"
 
 class App extends React.Component {
   constructor (props) {
@@ -16,22 +16,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        
+        <Navbar history = {this.props.history}/>
+
       </div>
     )
   }
-}
-
-const Navbar = (props) => {
-  return (
-    <div>
-
-      <a href = "https://www.eclimontessori.org" target = "_blank">
-        <img style = {{width : "100px"}} src = {logo} alt = "logo ECLI Montessori Thonon"/>
-      </a>
-    </div>
-  )
 }
 
 export default App;
---
+++ seguin/src/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './style.scss'
+const logo = "https://res.cloudinary.com/eclimontessori/image/upload/v1586343660/logo-small_rfd5z6.png"
+
+
+class Navbar extends Component {
+
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <a href = "https://www.eclimontessori.org" target = "_blank">
+          <img style = {{width : "100px"}} src = {logo} alt = "logo ECLI Montessori Thonon"/>
+        </a>
+        <button onClick={this.goTo.bind(this, 'seguin1niveau2')}>
+          Seguin1Niveau2
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
